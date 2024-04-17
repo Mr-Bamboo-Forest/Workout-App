ad76b019c6310410aa06db2b65a3b0828169b9ac
Bamboo Forest <113477194+Mr-Bamboo-Forest@users.noreply.github.com>
2024-04-17 19:12:35 +1000

    d

---
+++ script.js
@@ -0,0 +1,58 @@
+const startEl = document.getElementById("start");
+const stopEl = document.getElementById("stop");
+const resetEl = document.getElementById("reset");
+const timerEl = document.getElementById("timer");
+var currentExercise = document.getElementById("currentExercise").innerText;
+
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+randomWarmup = randomIntFromInterval(300, 420)
+
+let interval;
+let timeLeft = randomWarmup;
+let timerRunning = false;
+
+function updateTimer() {
+    if (currentExercise == "Workout Time!"){
+        document.getElementById("currentExercise").innerText = "Warm-Up!"
+    }
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
+    let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+
+    timerEl.innerHTML = formattedTime;
+}
+
+function startTimer() {
+    if (!timerRunning) {
+        timerRunning = true;
+        interval = setInterval(() => {
+            timeLeft--;
+            updateTimer();
+            if (timeLeft === 0) {
+                clearInterval(interval);
+                alert("Time's Up!");
+                timeLeft = 1500;
+                updateTimer();
+                timerRunning = false;
+            }
+        }, 1000);
+    }
+}
+
+function stopTimer() {
+    clearInterval(interval);
+    timerRunning = false;
+}
+
+function resetTimer() {
+    clearInterval(interval);
+    timeLeft = 1500;
+    updateTimer();
+    timerRunning = false;
+}
+
+startEl.addEventListener("click", startTimer);
+stopEl.addEventListener("click", stopTimer);
+resetEl.addEventListener("click", resetTimer);
